3f4850a05e2767133dfedf50711bd58050ea4635
Luuk Hendriks <mail@luukhendriks.eu>
2018-10-12 20:35:57 +0200

    show usage on invalid input, clean up

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,19 @@ use std::process::exit;
 use getopts::Options;
 
 const DEFAULT_RESOLVECONF_FN: &str = "/etc/resolv.conf";
-const HEADER: &str = "# the nameservers in this file were (possibly) reordered using resolvesolver on ";
+const HEADER: &str = "# the nameservers in this file were (possibly) reordered using resolvesolver on";
 
 fn main() {
     eprintln!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
-    opts.optflag("i", "", "modify file in-place");
+    opts.optflag("i", "", "modify in-place (otherwise output on stdout)");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(e) => {
             eprintln!("{}", e.to_string());
+            let brief = format!("Usage: {} RESOLVCONF_FILE [options]", env!("CARGO_PKG_NAME"));
+            print!("{}", opts.usage(&brief));
             exit(1);
         }
     };
@@ -38,15 +40,6 @@ fn main() {
         eprintln!("No such file: {}", resolveconf_fn);
         exit(1);
     }
-
-    //if let Ok(new_content) = parse_and_replace(&resolveconf_fn) {
-    //    if matches.opt_present("i") {
-    //        eprintln!("altering file in-place");
-    //        write_file(&resolveconf_fn, new_content).map_err(|e| eprintln!("error: {}", e));
-    //    } else {
-    //        new_content.iter().for_each(|l| println!("{}", l));
-    //    }
-    //}
 
     match parse_and_replace(&resolveconf_fn) {
         Ok(new_content) => {
@@ -91,14 +84,7 @@ fn parse_and_replace(resolveconf_fn: &str) -> Result<Vec<String>, io::Error> {
             lines_with_nameserver.push(line_no);
             let mut nameservers: Vec<String> = line.split_whitespace().skip(1).map(|s| s.to_string()).collect();
             assert!(!nameservers.is_empty());
-            //nameservers.sort_by(|a, b| sort_v6_over_v4(&a.as_str(), &b.as_str()));
-            nameservers.sort_by(|a, _b| {
-                if a.contains(':') {
-                    Ordering::Less
-                } else {
-                    Ordering::Greater
-                }
-            });
+            nameservers.sort_by(|a, b| sort_v6_over_v4(&a.as_str(), &b.as_str()));
             nested_nameservers.push(nameservers.to_vec());
         }
     }
@@ -119,7 +105,6 @@ fn parse_and_replace(resolveconf_fn: &str) -> Result<Vec<String>, io::Error> {
 
 fn refresh_header(lines: &mut Vec<String>) {
     if lines[0].contains(HEADER) {
-        eprintln!("replacing header");
         lines[0] = gen_header();
     } else {
         lines.insert(0, gen_header());
@@ -142,11 +127,11 @@ fn write_file(out_fn: &str, contents: Vec<String>) -> Result<(), io::Error> {
     Ok(())
 }
 
-//fn sort_v6_over_v4(a: &String, _b: &String) -> Ordering {
 fn sort_v6_over_v4<'s, 't>(a: &'s &str, _b: &'t &str) -> Ordering {
-    //naive first version to test
+    // naive sorting
+    // perhaps prioritizing global over link-local (or the other way around) makes sense
     if a.contains(':') {
-        // v6
+        // it's v6
         Ordering::Less
     } else {
         Ordering::Greater
